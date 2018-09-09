aae22949ad4c80ab431e61b771ed72aa86f7616d
Daniel-Cross <crossy1686@gmail.com>
2018-09-09 16:21:06 +0100

    added PUT request

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const helloWorld = require('./controllers/helloWorld');
 const createShoppingList = require('./controllers/createShoppingList');
 const getShoppingList = require('./controllers/getShoppingList');
+const updateShoppingList = require('./controllers/updateShoppingList');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/', helloWorld);
 app.post('/shopping-lists', createShoppingList);
-app.get('/shopping-lists:filename', getShoppingList);
+app.get('/shopping-lists/:filename', getShoppingList);
+app.put('/shopping-lists/:filename', updateShoppingList);
 
 app.listen(3000, () => console.log('App listening on port 3000'));
